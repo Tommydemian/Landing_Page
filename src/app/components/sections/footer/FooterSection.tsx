c19315d04f358f1403634dfc5ff9c4ae91fc7d94
HaiderPurple <haider@purplefire.io>
2025-03-02 05:08:38 +0200

    footer-complete

--- src/app/components/sections/footer/FooterSection.tsx
+++ src/app/components/sections/footer/FooterSection.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const FooterSection = () => {
   return (
     <Container className='my-section p-5 w-full'>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 border-b border-gray-500 py-5'>
         <Image className='col-span-1' src='/footer-logo.svg' alt='logo' width={100} height={100} />
         <div className='col-span-2 flex justify-between'>
             <div className='flex gap-10 w-full'>
@@ -27,6 +27,23 @@ const FooterSection = () => {
                 <Image src='/linkedin.svg' alt='instagram' width={20} height={20} />
             </a>
             </div>
+        </div>
+      </div>
+
+      <div className='flex justify-between py-5'>
+        <div className='flex gap-5 items-center'>
+            <div className='flex items-center gap-2'>
+                <Image src='/mail.svg' alt='logo' width={20} height={20} />
+                <span>hello@squareup.com</span>
+            </div>
+
+            <div className='flex items-center gap-2'>
+                <Image src='/call.svg' alt='logo' width={20} height={20} />
+                <span>+123 45687890</span>
+            </div>
+        </div>
+        <div>
+        © 2023 Aviren Studio. All rights reserved.
         </div>
       </div>
     </Container>
